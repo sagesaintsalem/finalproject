d86e55cda36908b8d81834e2aea428fe2dc04977
sagesaintsalem <85616342+sagesaintsalem@users.noreply.github.com>
2023-02-27 15:40:52 +0000

    Ports put in loop

--- project/src/main/java/com/seacleaver/example/project/ProjectApplication.java
+++ project/src/main/java/com/seacleaver/example/project/ProjectApplication.java
@@ -1,5 +1,6 @@
 package com.seacleaver.example.project;
 
+import com.seacleaver.example.project.models.Area;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,54 +14,75 @@ public class ProjectApplication {
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(ProjectApplication.class, args);
 
-
-
+		Area area = new Area(1);
 		String command = " ";
 		Scanner sc = new Scanner(System.in);
+//		area = 1;
+		boolean southVisited = false;
+		boolean westVisited = false;
+		boolean eastVisited = false;
+		boolean northVisited = false;
 
-		System.out.println("> Barnacles: ");
-		System.out.println("> Where shall we sail, Cap'n?");
-		command = sc.nextLine();
-
-		if(command.equals("south")){
+		while(area.getArea() == 1) {
 			System.out.println("> Barnacles: ");
-			System.out.println("> Ahoy! I see Port Rouge! \n");
-			TimeUnit.SECONDS.sleep(2);
-			System.out.println("> Patches: ");
-			System.out.println("> *SQUAWK* LAWFUL AND RICH! LAWFUL AND RICH! \n");
-			TimeUnit.SECONDS.sleep(2);
-			System.out.println("> Barnacles: ");
-			System.out.println("> Hmm...shall we raid them, Cap'n?");
+			System.out.println("> Where shall we sail, Cap'n?");
 			command = sc.nextLine();
-			if(command.equals("raid")||command.equals("yes")){
-				System.out.println("> Your crew yells in excitement!");
-				System.out.println("> You all launch a raid on Port Rouge!");
-				TimeUnit.SECONDS.sleep(3);
-				System.out.println("You plunder 2000 gold!");
-			} else if (command.equals("no")) {
-				System.out.println("> Barnacles: ");
-				System.out.println("> I see. Very well, Cap'n! ");
-			}
-		} else if(command.equals("west")){
-			System.out.println("> Barnacles: ");
-			System.out.println("> Ahoy! I see Port Raven! \n");
-			TimeUnit.SECONDS.sleep(2);
-			System.out.println("> Patches: ");
-			System.out.println("> *SQUAWK* HIDDEN TREASURES! \n");
-			TimeUnit.SECONDS.sleep(2);
-			System.out.println("> Barnacles: ");
-			System.out.println("> Hmm...shall we raid them, Cap'n?");
-			command = sc.nextLine();
-			if(command.equals("raid")||command.equals("yes")){
-				System.out.println("> Your crew yells in excitement!");
-				System.out.println("> You all launch a raid on Port Raven!");
-				TimeUnit.SECONDS.sleep(3);
-				System.out.println("You plunder 1000 gold!");
-			} else if (command.equals("no")) {
-				System.out.println("> Barnacles: ");
-				System.out.println("> I see. Very well, Cap'n! ");
-			}
 
+			if (command.equals("south")) {
+				area.setArea(2);
+				TimeUnit.SECONDS.sleep(3);
+				System.out.println("> Barnacles: ");
+				System.out.println("> Ahoy! I see Port Rouge! \n");
+				TimeUnit.SECONDS.sleep(2);
+				System.out.println("> Patches: ");
+				System.out.println("> *SQUAWK* LAWFUL AND RICH! LAWFUL AND RICH! \n");
+				TimeUnit.SECONDS.sleep(2);
+				System.out.println("> Barnacles: ");
+				System.out.println("> Hmm...shall we raid them, Cap'n?");
+				command = sc.nextLine();
+				if (command.equals("raid") || command.equals("yes")) {
+					System.out.println("> Your crew yells in excitement!");
+					System.out.println("> You all launch a raid on Port Rouge!");
+					TimeUnit.SECONDS.sleep(3);
+					System.out.println("You plunder 2000 gold!");
+					southVisited = true;
+					TimeUnit.SECONDS.sleep(3);
+					area.setArea(1);
+				} else if (command.equals("no")) {
+					System.out.println("> Barnacles: ");
+					System.out.println("> I see. Very well, Cap'n! ");
+					TimeUnit.SECONDS.sleep(3);
+					area.setArea(1);
+				}
+			} else if (command.equals("west")) {
+				area.setArea(3);
+				TimeUnit.SECONDS.sleep(3);
+				System.out.println("> Barnacles: ");
+				System.out.println("> Ahoy! I see Port Raven! \n");
+				TimeUnit.SECONDS.sleep(2);
+				System.out.println("> Patches: ");
+				System.out.println("> *SQUAWK* HIDDEN TREASURES! \n");
+				TimeUnit.SECONDS.sleep(2);
+				System.out.println("> Barnacles: ");
+				System.out.println("> Hmm...shall we raid them, Cap'n?");
+				command = sc.nextLine();
+				if (command.equals("raid") || command.equals("yes")) {
+					System.out.println("> Your crew yells in excitement!");
+					System.out.println("> You all launch a raid on Port Raven!");
+					TimeUnit.SECONDS.sleep(3);
+					System.out.println("You plunder 1000 gold!");
+					westVisited = true;
+					TimeUnit.SECONDS.sleep(3);
+					area.setArea(1);
+
+				} else if (command.equals("no")) {
+					System.out.println("> Barnacles: ");
+					System.out.println("> I see. Very well, Cap'n! ");
+					TimeUnit.SECONDS.sleep(3);
+					area.setArea(1);
+				}
+
+			}
 		}
 	}
 
---
+++ project/src/main/java/com/seacleaver/example/project/models/Area.java
@@ -0,0 +1,18 @@
+package com.seacleaver.example.project.models;
+
+public class Area {
+
+    private int area;
+
+    public Area(int area) {
+        this.area = area;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+    }
+}
